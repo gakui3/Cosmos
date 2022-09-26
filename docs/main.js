3078c920559dc3dc36cce18033f83c055058c6ae
gKirii <gaku.kirii@gmail.com>
2022-09-26 15:07:56 +0900

    Clean up

--- docs/main.js
+++ docs/main.js
@@ -1,24 +1,10 @@
 import * as BABYLON from "@babylonjs/core";
+import { Galaxy } from "./Galaxy";
 
-const canvas = document.getElementById("renderCanvas");
-const engine = new BABYLON.Engine(canvas);
-const mainScene = new BABYLON.Scene(engine);
-const mainCamera = new BABYLON.UniversalCamera("mainCamera", new BABYLON.Vector3(0, 0, 0), mainScene);
-const screenCamera = new BABYLON.UniversalCamera("subCamera", new BABYLON.Vector3(0, 0, -1), mainScene);
-
-const root = new BABYLON.TransformNode("root");
-const mainCameraRoot = new BABYLON.TransformNode("mainCameraRoot");
-
-mainCamera.attachControl(canvas, true);
-mainScene.clearColor = new BABYLON.Color3(0.0, 0.0, 0.0);
-
-const renderTarget = new BABYLON.RenderTargetTexture("depth", 1024, mainScene, true); // rendertargettextureの作成(unityでいうrendertexture)
-renderTarget.activeCamera = screenCamera; // rendertargettextureのアクティブカメラを指定
-mainScene.customRenderTargets.push(renderTarget); // rendertargettextureを有効化
-
-screenCamera.parent = root;
-mainCamera.parent = mainCameraRoot;
-mainCameraRoot.position = new BABYLON.Vector3(0, 0, -15);
+const Mode = {
+  floating: 0,
+  walking: 1,
+};
 
 let vlat = 0;
 let vlon = 0;
@@ -28,60 +14,17 @@ const alt = 8;
 const t = 0.03;
 const pi = 3.1415;
 const attenuationRate = 0.998;
-const acceleration = 0.015;
+const acceleration = 0.02;
+let mode = Mode.floating;
+let amount = 0;
+let earth, human, debug, screen, mainCamera, screenCamera, renderTarget, galaxy, floatingRoot, walkingRoot, screenRoot, mainCameraRoot, light;
 
-// camera.attachControl(canvas, true);
+const canvas = document.getElementById("renderCanvas");
+const engine = new BABYLON.Engine(canvas);
+const mainScene = new BABYLON.Scene(engine);
 
-const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), mainScene);
-light.intensity = 0.7;
-
-const material = new BABYLON.StandardMaterial("test", mainScene);
-material.diffuseTexture = new BABYLON.Texture("./assets/earth.png");
-material.ambientColor = new BABYLON.Color3(1, 1, 1);
-material.emissiveColor = new BABYLON.Color3(1, 1, 1);
-
-// earth
-const sphere = BABYLON.CreateSphere("sphere1", { segments: 20, diameter: 15 }, mainScene);
-sphere.material = material;
-sphere.rotate(BABYLON.Vector3.Right(), 3.14);
-renderTarget.renderList.push(sphere); // rendertargettextureに書き込むオブジェクトを指定
-
-// human
-const obj = BABYLON.CreateCapsule("obj", { height: 1, radius: 0.125 }, mainScene);
-
-obj.material = new BABYLON.StandardMaterial("objMat");
-
-obj.parent = root;
-root.position.y = -2;
-
-// screen
-const plane = BABYLON.CreatePlane("map", { width: 40, height: 20 }, mainScene);
-plane.position.z = 20;
-plane.position.y = 10;
-const rttMaterial = new BABYLON.StandardMaterial("RTT", mainScene);
-rttMaterial.emissiveTexture = renderTarget;
-rttMaterial.disableLighting = true;
-plane.material = rttMaterial;
-
-// particles
-const ps = new BABYLON.ParticleSystem("particles", 10000, mainScene);
-ps.particleTexture = new BABYLON.Texture("./assets/particle.png");
-const emitter = new BABYLON.SphereParticleEmitter();
-emitter.radius = 50;
-emitter.radiusRange = 0;
-ps.particleEmitterType = emitter;
-ps.emitRate = 500;
-ps.minSize = 0.15;
-ps.maxSize = 0.3;
-ps.addColorGradient(0, new BABYLON.Color4(0, 0, 0, 0.0));
-ps.addColorGradient(0.1, new BABYLON.Color4(1, 1, 1, 1.0));
-ps.addColorGradient(0.8, new BABYLON.Color4(1, 1, 1, 1.0));
-ps.addColorGradient(1.0, new BABYLON.Color4(0, 0, 0, 0.0));
-ps.minLifeTime = 5.0;
-ps.maxLifeTime = 20.0;
-// Blend mode : BLENDMODE_ONEONE, BLENDMODE_STANDARD, or BLENDMODE_ADD
-ps.blendMode = BABYLON.ParticleSystem.BLENDMODE_ADD;
-ps.start();
+init();
+addObject();
 
 // keyevent
 mainScene.onKeyboardObservable.add((kbInfo) => {
@@ -90,6 +33,7 @@ mainScene.onKeyboardObservable.add((kbInfo) => {
       switch (kbInfo.event.key) {
         case "w":
           vlon = vlon - acceleration * t;
+          amount = 0.01;
           break;
         case "a":
           vlat = vlat + acceleration * t;
@@ -100,41 +44,161 @@ mainScene.onKeyboardObservable.add((kbInfo) => {
         case "d":
           vlat = vlat - acceleration * t;
           break;
+        case "u":
+          if (mode === Mode.floating) {
+            walkingModeInit();
+          } else if (mode === Mode.walking) {
+            floatingModeInit();
+          }
+          break;
         default:
           break;
       }
       vlat = BABYLON.Scalar.Clamp(vlat, -10, 10);
       vlon = BABYLON.Scalar.Clamp(vlon, -10, 10);
+      break;
+    // case BABYLON.KeyboardEventTypes.KEYUP:
+    //   console.log("up");
+    //   break;
   }
 });
 
 function update () {
-  vlat = vlat * attenuationRate;
-  vlon = vlon * attenuationRate;
+  switch (mode) {
+    case Mode.floating: {
+      vlat = vlat * attenuationRate;
+      vlon = vlon * attenuationRate;
 
-  lat = lat + vlat * t;
-  lon = lon + vlon * t;
-  const phi = getDecimal(lat) * pi * 2;
-  const theta = BABYLON.Scalar.Clamp(getDecimal(lon) * pi, 0.2, pi - 0.2);
+      lat = lat + vlat * t;
+      lon = lon + vlon * t;
+      const phi = getDecimal(lat) * pi * 2;
+      const theta = BABYLON.Scalar.Clamp(getDecimal(lon) * pi, 0.2, pi - 0.2);
 
-  const x = alt * Math.sin(theta) * Math.sin(phi);
-  const y = alt * Math.cos(theta);
-  const z = alt * Math.sin(theta) * Math.cos(phi);
+      const x = alt * Math.sin(theta) * Math.sin(phi);
+      const y = alt * Math.cos(theta);
+      const z = alt * Math.sin(theta) * Math.cos(phi);
 
-  root.position = new BABYLON.Vector3(x, y, z);
-  root.lookAt(new BABYLON.Vector3(0, 0, 0));
+      floatingRoot.position = new BABYLON.Vector3(x, y, z);
+      floatingRoot.lookAt(new BABYLON.Vector3(0, 0, 0));
 
-  // mainCameraRoot.rotate(BABYLON.Vector3.Up, 45
-  const offset = 2.0;
-  mainCameraRoot.lookAt(root.position);
-  mainCameraRoot.position = BABYLON.Vector3.Lerp(mainCameraRoot.position, new BABYLON.Vector3(x, y, z).scale(offset), 0.005);
+      // mainCameraRoot.rotate(BABYLON.Vector3.Up, 45
+      const offset = 1.6;
+      mainCameraRoot.lookAt(floatingRoot.position);
+      mainCameraRoot.position = BABYLON.Vector3.Lerp(mainCameraRoot.position, new BABYLON.Vector3(x, y, z).scale(offset), 0.005);
+      break;
+    }
+    case Mode.walking: {
+      earth.rotate(BABYLON.Vector3.Right(), amount);
+      const p = floatingRoot.position.add(floatingRoot.up.scale(-5)).add(floatingRoot.forward.scale(-1));
+      mainCameraRoot.lookAt(walkingRoot.position);
+      mainCameraRoot.position = BABYLON.Vector3.Lerp(mainCameraRoot.position, p, 0.05);
+      screenRoot.lookAt(mainCameraRoot.position);
+      amount = 0;
+      break;
+    }
+    default:
+  }
+}
 
-  // console.log("root pos : " + root.position);
-  // console.log("maincam : " + mainCameraRoot.position);
+function init () {
+  mainCamera = new BABYLON.UniversalCamera("mainCamera", new BABYLON.Vector3(0, 0, 0), mainScene);
+  screenCamera = new BABYLON.UniversalCamera("subCamera", new BABYLON.Vector3(0, 0, -1), mainScene);
+  mainCameraRoot = new BABYLON.TransformNode("mainCameraRoot");
+
+  mainCamera.attachControl(canvas, true);
+  mainScene.clearColor = new BABYLON.Color3(0.0, 0.0, 0.1);
+
+  mainCameraRoot.position = new BABYLON.Vector3(0, 0, -13);
+
+  renderTarget = new BABYLON.RenderTargetTexture("depth", 1024, mainScene, true); // rendertargettextureの作成(unityでいうrendertexture)
+  renderTarget.activeCamera = screenCamera; // rendertargettextureのアクティブカメラを指定
+  mainScene.customRenderTargets.push(renderTarget); // rendertargettextureを有効化
+}
+
+function addObject () {
+  floatingRoot = new BABYLON.TransformNode("floatingRoot");
+  walkingRoot = new BABYLON.TransformNode("walkingRoot");
+  screenRoot = new BABYLON.TransformNode("screenRoot");
+
+  // earth
+  earth = BABYLON.CreateSphere("sphere1", { segments: 20, diameter: 15 }, mainScene);
+  const material = new BABYLON.StandardMaterial("earthMat", mainScene);
+  material.diffuseTexture = new BABYLON.Texture("./assets/earth.png");
+  material.ambientColor = new BABYLON.Color3(1, 1, 1);
+  material.emissiveColor = new BABYLON.Color3(1, 1, 1);
+  earth.material = material;
+  earth.rotate(BABYLON.Vector3.Right(), 3.14);
+  renderTarget.renderList.push(earth); // rendertargettextureに書き込むオブジェクトを指定
+
+  // human
+  human = BABYLON.MeshBuilder.CreateCylinder("cylinder", { diameterTop: 0.1, height: 0.8, diameterBottom: 0.25 });// BABYLON.CreateCapsule("obj", { height: 1, radius: 0.125 }, mainScene);
+  human.material = new BABYLON.StandardMaterial("objMat");
+  human.parent = floatingRoot;
+  floatingRoot.position.y = -2;
+
+  // debug obj
+  debug = BABYLON.CreateSphere("debug", { diameter: 0.5 }, mainScene);
+  debug.material = new BABYLON.StandardMaterial("debugMat");
+
+  // screen
+  screen = BABYLON.CreatePlane("map", { width: 40, height: 20 }, mainScene);
+  screen.parent = screenRoot;
+  screenRoot.position.z = 20;
+  screenRoot.position.y = 10;
+  const rttMaterial = new BABYLON.StandardMaterial("RTT", mainScene);
+  rttMaterial.emissiveTexture = renderTarget;
+  rttMaterial.disableLighting = true;
+  rttMaterial.backFaceCulling = false;
+  screen.material = rttMaterial;
+
+  // particles
+  galaxy = new Galaxy(mainScene);
+
+  // light
+  light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), mainScene);
+  light.intensity = 0.7;
+
+  // add root
+  screenCamera.parent = floatingRoot;
+  mainCamera.parent = mainCameraRoot;
 }
 
 function getDecimal (num) {
   return num - ((num >= 0) ? Math.floor(num) : Math.ceil(num));
+}
+
+function floatingModeInit () {
+  vlat = 0;
+  vlon = 0;
+  // screenRoot.rotate(screenRoot.right, 1.57);
+  walkingRoot.rotate(floatingRoot.right, 1.57);
+  human.parent = floatingRoot;
+  // human.rotate(BABYLON.Vector3.Right(), -1.57);
+  mode = Mode.floating;
+}
+
+function walkingModeInit () {
+  // for human
+  human.parent = null;
+  walkingRoot.position = BABYLON.Vector3.Zero();
+  human.parent = walkingRoot;
+  walkingRoot.position = floatingRoot.position;
+  walkingRoot.rotate(floatingRoot.right, -1.57);
+
+  // for screen
+  screenRoot.position = floatingRoot.position.add(floatingRoot.up.scale(25)).add(floatingRoot.forward.scale(3));
+  const dir = floatingRoot.position.add(floatingRoot.up);
+  debug.position = dir.scale(20);
+
+  // const axis0 = BABYLON.Vector3.Cross(dir.scale(-1), screenParent.forward);
+  // const rad = Math.acos(BABYLON.Vector3.Dot(dir.scale(-1), screenParent.forward) / (dir.scale(-1).length * screenParent.forward.length));
+  // const q0 = BABYLON.Quaternion.RotationAxis(axis0, rad);
+
+  // const q1 = BABYLON.Quaternion.RotationAxis()
+  // screenParent.rotation = walkingParent.rotation;
+  screenRoot.rotate(screenRoot.right, 1.57);
+
+  mode = Mode.walking;
 }
 
 // Render every frame
